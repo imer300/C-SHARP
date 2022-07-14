d44dcab4a27d34f6acbe07b9fdc2000ef8795b92
imer300 <98670396+imer300@users.noreply.github.com>
2022-07-13 19:56:31 -0600

    Add files via upload

---
+++ inventario en C# con POO/5-Propiedades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Bryan";
+            a.SegundoNombre = "Francisco";
+            
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Waldina";
+            b.SegundoNombre = "Zavala";
+            
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Hannia";
+
+            Alumno d = new Alumno("Billy", "Feranadez");
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+        }
+    }
+}
---
+++ inventario en C# con POO/6.Funciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "peper", "lopez");
+            Alumno b = new Alumno(2, "andrea", "lopez");
+
+            a.inactivarAlumno();
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine(" ");
+            
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+
+        }
+    }
+}
